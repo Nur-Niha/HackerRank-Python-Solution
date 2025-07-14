137c5a981f22552903b8d64a84ec8db0511ceb0a
Jannatun Nur Niha <jannatunnurniha@gmail.com>
2025-07-14 11:37:01 +0600

    Update if-else-sloution.py

--- if-else-sloution.py
+++ if-else-sloution.py
@@ -1,10 +1,15 @@
+n = int(input().strip())  
+# Read an integer from user input, remove any leading/trailing spaces, and convert to int
 
-n = int(input().strip())
 if n % 2 != 0 and n < 20:
-      print("Wierd")
+    # If n is odd and less than 20, print "Wierd"
+    print("Wierd")
 elif n % 2 == 0 and 2 <= n <= 5:
-      print("Not Wierd")
+    # If n is even and between 2 and 5 (inclusive), print "Not Wierd"
+    print("Not Wierd")
 elif n % 2 == 0 and 6 <= n <= 20:
-      print("Wierd")
+    # If n is even and between 6 and 20 (inclusive), print "Wierd"
+    print("Wierd")
 else:
-      print("Not Wierd")
+    # All other cases (even numbers greater than 20), print "Not Wierd"
+    print("Not Wierd")
