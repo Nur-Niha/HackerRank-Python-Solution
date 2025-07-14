10301b8906d9f0377bf5adf9ba2d13d8c142fe56
Jannatun Nur Niha <jannatunnurniha@gmail.com>
2025-07-14 11:59:46 +0600

    Add files via upload

---
+++ print the integer 1 through n as strings.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(1,n+1):
+    print(i, end='')    
+
+
+# We use end='' when we want to print in one line.
+# If there is no space between the quotes,
+# we get the output with no space between two characters.
+
+# In this program, our task is to 
+# print integers 1 through n as a single string.
