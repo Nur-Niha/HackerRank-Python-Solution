0b4da587495cf8a58f7e76b218be69bf1cd992a9
Jannatun Nur Niha <jannatunnurniha@gmail.com>
2025-07-14 10:39:25 +0600

    Update hackerRank_list.py

--- hackerRank_list.py
+++ hackerRank_list.py
@@ -1,20 +1,24 @@
-N = int(input())
-mylist = []
+# A program that takes an integer N as input (number of commands),
+# then reads N commands and performs list operations accordingly.
+
+
+N = int(input())                                          # read the number of commands
+mylist = []                                               # initialize an empty list
 
 for _ in range(N):
-    command = input().split()
+    command = input().split()                             # splits a input line in a list of words and stores in command                 
     if command[0] == "insert":
-        mylist.insert(int(command[1]), int(command[2]))
+        mylist.insert(int(command[1]), int(command[2]))   # insert element at a specified position
     elif command[0] == "print":
-        print(mylist)
+        print(mylist)                                     # prints the list 
     elif command[0] == "remove":
-        mylist.remove(int(command[1]))
+        mylist.remove(int(command[1]))                    # removes the specified element 
     elif command[0] == "append":
-        mylist.append(int(command[1]))
+        mylist.append(int(command[1]))                    # add specified element at the end                   
     elif command[0] == "sort":
-        mylist.sort()
+        mylist.sort()                                     # sorts the list in ascending order
     elif command[0] == "pop":
-        mylist.pop()
+        mylist.pop()                                      # removes the last item
     elif command[0] == "reverse":
-        mylist.reverse()
+        mylist.reverse()                                  # reverses the list
          
